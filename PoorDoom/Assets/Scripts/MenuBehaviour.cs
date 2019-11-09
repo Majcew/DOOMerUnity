3b50099aa274ccfb7a87ee53b84edec8619479d8
Majcew <48514316+Majcew@users.noreply.github.com>
2019-11-09 15:14:22 +0100

    Added menu for tests
    play wybiera następną scenę z puli (trzeba stworzyć widok ze wszystkimi levelami). Opcje nic nie robią

---
+++ PoorDoom/Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void EndGame()
+    {
+        Debug.Log("Let's see if it quits");
+        Application.Quit();
+    }
+}
