aea240813b80c078f6b6de5116b5a475c1174de3
Majcew <48514316+Majcew@users.noreply.github.com>
2019-11-14 19:14:13 +0100

    Added testing platform for scripts
    Dodałem skrypty do chodzenia,strzelania oraz hp dla przeciwników

---
+++ PoorDoom/Assets/Scripts/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float damage = 10f;
+    public Camera fpscam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootBullet();
+        }
+    }
+
+    private void ShootBullet()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
