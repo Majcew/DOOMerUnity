400c945c89114c6a06cb75e6cfb08bba1ec5c8dc
Mirosław Witkowski <kapitanmirek@wp.pl>
2019-12-19 21:23:07 +0100

    Fix boss and add functions, add move, attack, damage, idle animation and function, add dead sound, add hit sound. Refactor scripts.

---
+++ PoorDoom/Assets/Scripts/BossHealth.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+    BossManager mng;
+
+    public float startingHealth = 400f;
+    public float currentHealth;
+
+    public AudioSource bossDeathAudio;
+
+    public bool isDead = false;
+    bool isSinking;
+
+    void Awake()
+    {
+        mng = GetComponent<BossManager>();
+
+        currentHealth = startingHealth;
+    }
+
+
+    public void TakeDamage(float amount/*, Vector3 hitPoint*/)
+    {
+        if(isDead == false)
+        {
+            Debug.Log("Ała");
+            mng.animator.SetTrigger("Damage");
+            currentHealth -= amount;
+            if (currentHealth <= 0)
+            {
+                isDead = true;
+                Death();
+            }
+        }
+
+    }
+
+    void Death()
+    {
+        Debug.Log("Dead");
+        mng.animator.SetBool("NotLive", true);
+        bossDeathAudio.Play();
+    }
+
+
+
+}
---
+++ PoorDoom/Assets/Scripts/BossMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    BossManager mng;
+
+    Transform playerTransform;
+
+    UnityEngine.AI.NavMeshAgent navMesh;
+
+    public bool playerInRange = false;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == mng.player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == mng.player)
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Awake()
+    {
+        mng = GetComponent<BossManager>();
+        
+        playerTransform = mng.player.transform;
+
+        navMesh = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+
+    public void goAndFollow()
+    {
+        //Debug.Log("ide");
+        navMesh.enabled = true;
+        navMesh.SetDestination(playerTransform.position);
+        mng.animator.SetBool("Run", true);
+    }
+
+    public void stop()
+    {
+        //Debug.Log("stoje");
+        navMesh.enabled = false;
+        mng.animator.SetBool("Run", false);
+    }
+}
